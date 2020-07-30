295b1d0dc6e9d1906a12f849435be183506d560f
Gabriel Tan <tangabrielkl@gmail.com>
2020-07-30 13:50:50 -0400

    Added project to calculate the standard deviation of a Collection of type double

---
+++ Math.NETTests/Collection.GenericTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathDotNET.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathDotNET.Collections.Generic.Tests
+{
+    [TestClass()]
+    public class ExtensionTests
+    {
+        [TestMethod()]
+        public void StandardDeviationPTest()
+        {
+            List<double> list = new List<double>(new double[] { 2,4,4,4,5,5,7,9});
+            Assert.AreEqual(2, list.StandardDeviationP());
+        }
+    }
+}
---
+++ MathDotNET.Collections.Generic/Extension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MathDotNET.Collections.Generic
+{
+    public static class Extension
+    {
+        /// <summary>
+        /// Returns the population standard deviation of values
+        /// </summary>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public static double StandardDeviationP(this IEnumerable<double> values)
+        {
+            double avg = values.Average();
+            return Math.Sqrt(values.Average(v => Math.Pow(v - avg, 2)));
+        }
+    }
+}
